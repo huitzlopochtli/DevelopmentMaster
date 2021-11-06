fa0f872d56355903096a66f18ca75f730ea1ca30
Hashib Khondokar <khondokar.hash@gmail.com>
2021-11-07 01:01:27 +0900

    node command line argument

--- Javascript/node_study/notesapp/app.js
+++ Javascript/node_study/notesapp/app.js
@@ -6,8 +6,20 @@
 import validator from 'validator';
 import chalk from 'chalk';
 
-
 console.log(validator.isEmail('test'));
 console.log(validator.isURL('peeyalk.com'));
 
 console.log(chalk.red.inverse.bold('Error'));
+
+/*
+	Command Line Arguments
+*/
+const command = process.argv[2];
+
+switch (command) {
+  case 'add':
+    console.log(command);
+    break;
+  default:
+    console.log(chalk.red.bold.inverse('Command not found'));
+}
